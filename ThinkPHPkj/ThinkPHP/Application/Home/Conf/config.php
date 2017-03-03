6d7bb94f27f4fc7992681eda5a822a0bd81d5198
PC-20140101QGPO\Administrator <314850412@qq.com>
2017-03-03 10:26:21 +0800

    Revert "no message"
    This reverts commit 8e2852858d01bc06bc74f7c1ff727a6b54a73848.

--- ThinkPHPkj/ThinkPHP/Application/Home/Conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-	//'DEFAULT_C_LAYER'=>'action',//修改控制器名称  原Controller
-    //'SESSION_AUTO_START' => true, //是否开启session 默认是true
-    // 'URL_MODEL'          => '2', //URL模式  'URL_MODEL'             =>  1,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
-    // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
-
-);
