09da2ab1ca8f478fd677f9c59078b8ca9ae11e50
PC-20140101QGPO\Administrator <314850412@qq.com>
2017-03-03 10:09:53 +0800

    Revert "这是梦想的开始，转盘功能 TP3.2.3 php3.0以上"
    This reverts commit a8af346edde6d5373d9d71691679f37d1f43770d.

--- ThinkPHPkj/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++
@@ -1,5 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><form action="/my/ThinkPHPkj/ThinkPHPkj/index.php/Home/Index/login" method="post">
-    账号: <input type="text" name="user_name">
-    密码：<input type="password" name="user_pass">
-    <input type="submit">
-</form>
